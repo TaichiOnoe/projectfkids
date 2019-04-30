78897124502ae8d604c95f1b49b8bd8bc7390a49
Umijinko <TaichiOnoe@users.noreply.github.com>
2019-05-01 01:06:42 +0900

    init

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+<div>
+  <v-container>
+    <v-layout
+    text-xs-center>
+    <v-flex>
+        <v-avatar
+        :size = 300>
+      <img
+        src="https://cdn.vuetifyjs.com/images/john.jpg"
+        alt="myavatar"
+      >
+    </v-avatar>
+    </v-flex>
+    </v-layout>
+  </v-container>
+
+  </div>
+</template>
